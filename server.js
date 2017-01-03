0508ba55a0cff71d4da5da6e30d04fcd3087ecb8
Todd Chaffee <tc@toddbiz.com>
2017-01-03 10:18:07 +0000

    Timestamp microservice is working.

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:dateParam', function (req, res) {
+  var resObj = {};  
+  var resDate;
+  var options = {
+    year: "numeric", 
+    month: "long",
+    day: "numeric"
+  };
+  
+  if (isNaN(req.params.dateParam)) {
+    resDate = new Date(req.params.dateParam);
+  } else {
+    resDate = new Date(req.params.dateParam * 1000);
+  }
+
+  resObj.unix = resDate / 1000;
+  
+  if (resObj.unix) {
+    resObj.natural = resDate.toLocaleDateString('en-us', options);
+  } else {
+    resObj.natural = null;
+  }
+  
+  res.send(resObj);
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Timestamp app listening on port 8080.')
 })
